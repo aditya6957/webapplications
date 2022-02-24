a6331c8eb4a2e6fdaab212fad248506782a7127d
Aditya Devraj <adityadevraj20@gmail.com>
2022-02-24 14:16:32 +0530

    commit

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.secret_key = 'secret'
 
 @app.route('/')
@@ -9,9 +9,9 @@ def HomePage():
 @app.route('/+', methods = ['POST', 'GET'])
 def addition():
     add = ''
-    if request.method=='POST' and 'Second number' in request.form and 'First number' in request.form:
-        SN = float(request.form.get('Second number'))
-        FN = float(request.form.get('First number'))
+    if request.method=='POST' and 'second' in request.form and 'first' in request.form:
+        SN = float(request.form.get('second'))
+        FN = float(request.form.get('first'))
         add = (SN + FN)
     return render_template('add.html', add=add)    
 
